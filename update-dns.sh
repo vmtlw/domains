e2a34264d61c071774f4a18d30ab1560d7b5c20d
rooty <admin@mail.com>
2025-10-12 00:52:01 +0300

    новый файл:    update-dns.sh

---
+++ update-dns.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+git_tmp_dir=/tmp/domains
+git clone git@github.com:vmtlw/domains.git $git_tmp_dir
+domain=$1
+if grep -w $domain $git_tmp_dir/dnsmasq.lst; then 
+	echo "dns $domain already exists"
+else
+	echo "nftset=/$domain/4#inet#fw4#vpn_domains" $git_tmp_dir/domains.git
+	sort $git_tmp_dir/dnsmasq.lst > /tmp/sorted
+	mv /tmp/sorted $git_tmp_dir/dnsmasq.lst
+	echo "dns $domain added !"
+fi
+
+cd ~
+cd $git_tmp_dir;
+git add dnsmasq.lst
+git commit -m "added $domain"
+git push
+cd -
+
