b72b0f49aed7c5cc5062ddc1b2aa0716cbaf3877
vmtlw <63434489+vmtlw@users.noreply.github.com>
2026-01-02 15:52:33 +0300

    изменено:      update-dns.sh

--- update-dns.sh
+++ update-dns.sh
@@ -1,25 +1,33 @@
 #!/usr/bin/bash
-set +x 
-git_tmp_dir=/tmp/domains-$(date +%F-%M-%S)
-[[ -d $git_tmp_dir/.git ]] && rm -rf $git_tmp_dir
-git clone git@github.com:vmtlw/domains.git $git_tmp_dir
-domain=$1
-if grep -w $domain $git_tmp_dir/dnsmasq.lst; then 
-	echo "dns $domain already exists"
-else   
-	echo "nftset=/$domain/4#inet#fw4#vpn_domains" >> $git_tmp_dir/dnsmasq.lst
-	sort $git_tmp_dir/dnsmasq.lst > /tmp/sorted
-	mv /tmp/sorted $git_tmp_dir/dnsmasq.lst
-	echo "dns $domain added !"
+set -e
+
+git_tmp_dir=/tmp/domains-$(date +%F-%H-%M-%S)
+git clone git@github.com:vmtlw/domains.git "$git_tmp_dir"
+
+changed=0
+
+for domain in "$@"; do
+    if grep -qw "$domain" "$git_tmp_dir/dnsmasq.lst"; then
+        echo "dns $domain already exists"
+    else
+        echo "nftset=/$domain/4#inet#fw4#vpn_domains" >> "$git_tmp_dir/dnsmasq.lst"
+        echo "dns $domain added"
+        changed=1
+    fi
+done
+
+if [[ $changed -eq 1 ]]; then
+    sort -u "$git_tmp_dir/dnsmasq.lst" -o "$git_tmp_dir/dnsmasq.lst"
+
+    cd "$git_tmp_dir"
+    git add dnsmasq.lst
+    git commit -m "added domains: $*"
+    git push
+
+    ssh 10.0.0.1 service getdomains restart
+else
+    echo "no changes"
 fi
 
-cd ~
-cd $git_tmp_dir;
-git add dnsmasq.lst
-git commit -m "added $domain"
-git push
-cd -
-ssh 10.0.0.1 service getdomains restart
+rm -rf "$git_tmp_dir"
 
-rm -rf $git_tmp_dir
-set -x
