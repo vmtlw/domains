752db9fc82070e1c8a240b96dbd04d66bda2fec9
vmtlw <63434489+vmtlw@users.noreply.github.com>
2026-09-12 15:14:16 +0300

    прогнать каждый аргумент через sed, убрав схему и всё после первого /

--- update-dns.sh
+++ update-dns.sh
@@ -5,13 +5,29 @@ git_tmp_dir=/tmp/domains-$(date +%F-%H-%M-%S)
 git clone git@github.com:vmtlw/domains.git "$git_tmp_dir"
 
 changed=0
+added_domains=()
 
-for domain in "$@"; do
+for url in "$@"; do
+    # Убираем http:// или https://
+    domain="${url#http://}"
+    domain="${domain#https://}"
+
+    # Убираем путь, query string и fragment
+    domain="${domain%%/*}"
+    domain="${domain%%\?*}"
+    domain="${domain%%#*}"
+
+    if [[ -z "$domain" ]]; then
+        echo "invalid URL: $url"
+        continue
+    fi
+
     if grep -qw "$domain" "$git_tmp_dir/dnsmasq.lst"; then
         echo "dns $domain already exists"
     else
         echo "nftset=/$domain/4#inet#fw4#vpn_domains" >> "$git_tmp_dir/dnsmasq.lst"
         echo "dns $domain added"
+        added_domains+=("$domain")
         changed=1
     fi
 done
@@ -21,7 +37,7 @@ if [[ $changed -eq 1 ]]; then
 
     cd "$git_tmp_dir"
     git add dnsmasq.lst
-    git commit -m "added domains: $*"
+    git commit -m "added domains: ${added_domains[*]}"
     git push
 
     ssh 10.0.0.1 service getdomains restart
